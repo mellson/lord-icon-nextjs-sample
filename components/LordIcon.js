9b8ce4ec28d021f4307e94616c6dfb1d01a9faa7
Anders Bech Mellson <anders@bechmellson.com>
2020-04-15 11:20:38 +0200

    Update LordIcon.js

--- components/LordIcon.js
+++ components/LordIcon.js
@@ -3,5 +3,5 @@ import {defineLordIconElement} from 'lord-icon-element';
 
 defineLordIconElement(lottie.loadAnimation);
 
-const LordIcon = () => <lord-icon animation="hover" src="/1-cloud-outline.json" />
+const LordIcon = () => <lord-icon animation="hover" src="https://cdn.jsdelivr.net/gh/tomwilusz/lord-icon-element/examples/icons/13-pizza-solid.json" />
 export default LordIcon
